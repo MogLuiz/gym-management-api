676d3c9db6615b93b3b59647917523f2595a0dc9
Luiz Henrique <luizhjramos@outlook.com>
2023-03-21 05:54:30 -0300

    chore: envs validator with zod

---
+++ src/env/index.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import { envSchema } from "../schemas";
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("❌ Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,6 @@
+import { env } from "./env";
 import { app } from "./app";
 
-app.listen({ host: "0.0.0.0", port: 3333 }).then(() => console.log('🚀 HTTP Server Running!'))
+app
+  .listen({ host: "0.0.0.0", port: env.PORT })
+  .then(() => console.log("🚀 HTTP Server Running!"));
