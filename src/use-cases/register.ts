09daa3af672f527ab53898b9820ae9f4534772ec
Luiz Henrique <luizhjramos@outlook.com>
2023-03-31 08:07:10 -0300

    feat: add user repository interface to implements user repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,6 +1,6 @@
-
 import { hash } from 'bcryptjs'
 import { prisma } from '@/lib/prisma'
+import { IUsersRepository } from '@/repositories/users-repository'
 
 interface IRegisterUseCaseParams {
   name: string;
@@ -9,27 +9,20 @@ interface IRegisterUseCaseParams {
 }
 
 export class RegisterUseCase {
-    constructor(private usersRepository: any){}
+    constructor(private usersRepository: IUsersRepository) {}
 
-    async execute({
-        email,
-        name,
-        password,
-    }: IRegisterUseCaseParams) {
+    async execute({ email, name, password }: IRegisterUseCaseParams) {
         const password_hash = await hash(password, 6)
         const userWithSameEmail = await prisma.user.findUnique({
             where: {
                 email,
             },
         })
-    
+
         if (userWithSameEmail) {
             throw new Error('E-mail alread exists')
         }
-    
-    
-    
+
         await this.usersRepository.create({ name, email, password_hash })
     }
 }
-
