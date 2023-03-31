5a8d85404d5df2d695a1a720f03ff55ab212362a
Luiz Henrique <luizhjramos@outlook.com>
2023-03-31 20:40:01 -0300

    refactor: apply in-memory test design pattern structure inside use-cases test layer

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,25 +1,13 @@
 import { compare } from 'bcryptjs'
 import { expect, describe, it } from 'vitest'
 import { RegisterUseCase } from './register'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 
 describe('Register Use Case', () => {
     it('should hash user password upon registration', async () => {
         const password = '123456'
-        const registerUseCase = new RegisterUseCase({
-            async findByEmail() {
-                return null
-            },
-
-            async create(data) {
-                return {
-                    id: 'user-1',
-                    name: data.name,
-                    email: data.email,
-                    password_hash: data.password_hash,
-                    created_at: new Date(),
-                }
-            },
-        })
+        const usersRepository = new InMemoryUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
 
         const { user } = await registerUseCase.execute({
             name: 'John Doe',
