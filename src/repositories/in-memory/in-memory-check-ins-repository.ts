ced2eaf4f69e21d9760212fca6c000935b484a0a
Luiz Henrique <luizhjramos@outlook.com>
2023-04-02 14:59:48 -0300

    feat: add check-ins repository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,12 +1,11 @@
 import { CheckIn, Prisma } from '@prisma/client'
 import { ICheckInsRepository } from '../check-ins-repository'
 import { randomUUID } from 'node:crypto'
+import dayjs from 'dayjs'
 
 export class InMemoryCheckInsRepository implements ICheckInsRepository {
-
     public items: CheckIn[] = []
 
-    
     async create(data: Prisma.CheckInUncheckedCreateInput) {
         const checkIn = {
             id: randomUUID(),
@@ -19,5 +18,24 @@ export class InMemoryCheckInsRepository implements ICheckInsRepository {
         this.items.push(checkIn)
 
         return checkIn
+    }
+
+    async findByUserIdOnDate(userId: string, date: Date) {
+        const startOfTheDay = dayjs(date).startOf('date')
+        const endOfTheDay = dayjs(date).endOf('date')
+
+        const checkInOnSameDate = this.items.find((checkIn) => {
+            const checkInDate = dayjs(checkIn.created_at)
+            const isOnSameDate =
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay)
+
+            return checkIn.user_id === userId && isOnSameDate
+        })
+
+        if (!checkInOnSameDate) {
+            return null
+        }
+
+        return checkInOnSameDate
     }
 }
