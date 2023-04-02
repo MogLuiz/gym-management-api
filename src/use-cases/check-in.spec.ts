879e1ae5efeca0036c26551686ef5c5b1a2e9f14
Luiz Henrique <luizhjramos@outlook.com>
2023-04-02 11:11:03 -0300

    test: ensure not be able to check in twice in the same day

--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,7 +1,8 @@
-import { expect, describe, it, beforeEach } from 'vitest'
+import { expect, describe, it, beforeEach, vi, afterEach } from 'vitest'
 
 import { CheckInUseCase } from '@/use-cases/check-in'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory'
+
 
 const fakeCheckIn = {
     gymId: 'gym-01',
@@ -15,6 +16,12 @@ describe('Check-in Use Case', () => {
     beforeEach(() => {
         checkInsRepository = new InMemoryCheckInsRepository()
         sut = new CheckInUseCase(checkInsRepository)
+
+        vi.useFakeTimers()
+    })
+
+    afterEach(() => {
+        vi.useRealTimers()
     })
 
     it('should be able to check in', async () => {
@@ -23,5 +30,13 @@ describe('Check-in Use Case', () => {
         expect(checkIn.id).toEqual(expect.any(String))
         expect(checkIn.gym_id).toEqual(fakeCheckIn.gymId)
         expect(checkIn.user_id).toEqual(fakeCheckIn.userId)
+    })
+
+    it('should not be able to check in twice in the same day', async () => {
+        vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0))
+
+        await sut.execute(fakeCheckIn)
+
+        await expect(() => sut.execute(fakeCheckIn)).rejects.toBeInstanceOf(Error)  
     })
 })
