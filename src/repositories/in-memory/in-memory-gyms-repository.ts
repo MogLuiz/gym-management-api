0361ab96a9ac95f2c7a1bb2119e82a8e1a61aa08
Luiz Henrique <luizhjramos@outlook.com>
2023-04-03 06:23:42 -0300

    feat: create gym functionality on the repository

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
-import { Gym } from '@prisma/client'
+import { Gym, Prisma } from '@prisma/client'
 
 import { IGymsRepository } from '../gyms-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryGymsRepository implements IGymsRepository {
     public items: Gym[] = []
@@ -11,6 +12,22 @@ export class InMemoryGymsRepository implements IGymsRepository {
         if (!gym) {
             return null
         }
+
+        return gym
+    }
+
+    async create(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            created_at: new Date(),
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+        }
+
+        this.items.push(gym)
 
         return gym
     }
