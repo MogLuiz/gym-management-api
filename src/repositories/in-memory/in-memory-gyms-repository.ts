f27136ad06bae2b39db1a2f97b35011e5351149e
Luiz Henrique <luizhjramos@outlook.com>
2023-04-09 12:49:13 -0300

    test: ensure be able to fetch nearby gyms

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -43,7 +43,7 @@ export class InMemoryGymsRepository implements IGymsRepository {
                 },
             )
     
-            return distance < 10
+            return distance < 10000
         })
     }
 
