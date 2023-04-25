950317375abda4aab80a35eaca004f27388acba0
Luiz Henrique <luizhjramos@outlook.com>
2023-04-25 19:12:01 -0300

    test: ensure be able to get user profile

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -2,6 +2,12 @@ import request from 'supertest'
 import { app } from '@/app'
 import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
+const userMock = {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: '123456',
+}
+
 describe('Register (e2e)', () => {
     beforeAll(async () => {
         await app.ready()
@@ -12,11 +18,7 @@ describe('Register (e2e)', () => {
     })
 
     it('should be able to register', async () => {
-        const response = await request(app.server).post('/users').send({
-            name: 'John Doe',
-            email: 'johndoe@example.com',
-            password: '123456',
-        })
+        const response = await request(app.server).post('/users').send(userMock)
 
         expect(response.statusCode).toEqual(201)
     })
