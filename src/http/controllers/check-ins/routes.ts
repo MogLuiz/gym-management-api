b722fe8bf199e99030eed8ab0e8a814878c73cee
Luiz Henrique <luizhjramos@outlook.com>
2023-04-25 22:49:45 -0300

    feat: add history controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -1,9 +1,14 @@
 import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
-import {  } from '.'
+import { create, history } from '.'
 
 export async function checkInsRoutes(app: FastifyInstance) {
     // Add verifyJWT middleware for all requests
     app.addHook('onRequest', verifyJWT)
 
+    app.get('/check-ins/history', history)
+    app.get('/check-ins/metrics', metrics)
+
+    app.post('/gyms/:gymId/check-ins', create)
+    app.patch('/check-ins/:checkInId/validate', validate)
 }
